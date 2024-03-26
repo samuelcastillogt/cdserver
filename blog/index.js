31bcacb5ae48e136481725247b4ffda524c722ab
Samuel Castillo <samuelcastillo@MacBook-Pro-de-Samuel.local>
2024-03-26 16:16:02 -0600

    integracion de endpoint de blog

--- blog/index.js
+++ blog/index.js
@@ -2,10 +2,7 @@ import axios from "axios"
 
 export const getAllData = async()=>{
     const urls = []
-    const response = await axios.get("https://www.googleapis.com/blogger/v3/blogs/6035337752081805288/posts?key=AIzaSyCUbCHfwnO1P8oRvK1iHASDBaByyvPuEmE")
-
-    const data = DOMParser().parseFromString(response.data.items[0].content, "text/xml")
-    console.log(data)    
+    const response = await axios.get("https://www.googleapis.com/blogger/v3/blogs/6035337752081805288/posts?key=AIzaSyCUbCHfwnO1P8oRvK1iHASDBaByyvPuEmE")  
     response.data.items.forEach((i)=> urls.push({url: i.url, title: i.title}))
     return urls
 }
